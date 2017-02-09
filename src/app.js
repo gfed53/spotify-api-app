e41e647765b3f31b1cb50fe6c3bb02ae64df5228
gfed53 <gfederico7153@gmail.com>
2017-02-09 14:31:15 -0500

    Using CDN links for demo purposes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,5 @@
 (function(){
 	angular
 	.module('myApp', ['ngAnimate', 'ngMaterial', 'spotify'])
-	
-	.config(["$httpProvider", "SpotifyProvider", ($httpProvider, SpotifyProvider) => {
-		SpotifyProvider.setClientId("42a54f0910e3426f81c2c184e67b9294");
-	}])
 
 })();
