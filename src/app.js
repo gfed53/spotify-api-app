dbebc0961d41088de65a7f611955b07bc5d3ce10
gfed53 <gfederico7153@gmail.com>
2017-08-19 12:50:29 -0400

    Minor style tweaks, code cleanup

--- src/app.js
+++ src/app.js
@@ -7,8 +7,6 @@
 	.run(['spAPIKeys', 'spGetToken', (spAPIKeys, spGetToken)=>{
 	spAPIKeys.init()
 		.then((obj)=>{
-			console.log('Apis obj',obj);
-
 			//Use client ID to fetch auth token if needed
 			spGetToken.token = spGetToken.get();
 		});		
--- src/lib/my-func.js
+++ src/lib/my-func.js
@@ -1,4 +1,4 @@
 // Returns a random item from an array
 Array.prototype.getRandom = function(){
 	return this[Math.floor(Math.random()*this.length)];
-}
+};
